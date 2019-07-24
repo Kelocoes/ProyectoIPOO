021576a0ed655d27fd6999ac3106e51de64744ea
Joules17 <52895213+Joules17@users.noreply.github.com>
2019-07-23 21:49:53 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -3,16 +3,18 @@
 
 class Board
 {
-Public:
+public:
   Board();
   ~Board();
-  virtual void Matrix();
-  virtual void Match();//Abstraction
-  virtual void fill();//Need a verifier of where the match is happening
-  virtual void Move();
+  virtual void Matrix();  // It includes a Matrix where gems are gonna be located
+  virtual void Match();   // Abstraction where all matches movements are included
+  virtual void fill();    // Need a verifier of where the match is happening, then fill the void boxes
+  virtual void Move();    // Indicates to gems how to move around the board 
 
-Protected:
-Private:
+protected:
+  //
+private:
+  //
 
 };
 #endif

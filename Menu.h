c8c26d2ecf013d50332c3036aabdb5e0bf0e927c
Joules17 <52895213+Joules17@users.noreply.github.com>
2019-07-23 21:41:52 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -6,13 +6,16 @@ class Menu
 public:
   Menu();
   ~Menu();
-  virtual void ShowBoard();
-  virtual void LoadGame(archivo);
-  virtual void SaveGame();
-  virtual void Info();
+  // Here we are defining functions from class called Menu
+  // We gotta use virtual to work with polymorphism 
+  virtual void ShowBoard(); 
+  virtual void LoadGame(file); //   Upload a txt file to load a level
+  virtual void SaveGame(); //       Save the current progress from the user 
+  virtual void Help(); //           Let you know how to play the game (controls)
 
 protected:
+//
 private:
-
+//
 };
 #endif

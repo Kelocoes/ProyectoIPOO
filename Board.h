73143d860a780baa913c2e44ee0b8eec412a55c3
Joules17 <52895213+Joules17@users.noreply.github.com>
2019-09-08 15:37:18 -0500

    DOXYGEN
    Some comments were added for Doxywizard.

--- Board.h
+++ Board.h
@@ -1,3 +1,11 @@
+/**
+* @file Board.h
+* @version 1.0
+* @date 08/09/2019
+* @author Grupo04
+* @title Tablero
+* @brief Functions who helps playing and moving around the Matrix 
+*/
 #ifndef _BOARD_H_
 #define _BOARD_H_
 #include "Gem.h"
@@ -8,19 +16,83 @@
 class Board
 {
 public:
+  /**
+  * @brief  Board() Builder method. Starts Gems and all the movements
+  * @param void
+  * @return void
+  */
   Board(); // Builder 
+
+  /**
+  * @brief  ~Board() breaker
+  * @param void
+  * @return void
+  */
   ~Board();
+  /**
+  * @brief  SetMatrix()
+  * @param void
+  * @return void
+  */
    void SetMatrix();
+   /**
+  * @brief  Match() Erases gems and update the score 
+  * @param void
+  * @return void
+  */
    void Match(); // Helps to erase gems and update score 
+
+/**
+  * @brief  MatchColumn() Auxiliar function of Match. Works with possibleMatchs
+  * @param void
+  * @return void
+  */
+
    void MatchColumn(); // Auxiliar function of Match()
+
+   /**
+  * @brief  MatchLine() Auxiliar function of Match. Works with possibleMatchs
+  * @param void
+  * @return void
+  */
+
    void MatchLine(); // Auxiliar function of Match()
+
+/**
+  * @brief  Move() Important function. It lets you move the gems
+
+  * @param void
+  * @return void
+  */
 
    void Move();//Movement of the gems
 
 //
+
+/**
+  * @brief  checkEmpty() Read matrix and check when there is a void on the matrix. Add gravity to board.
+  * @param void
+  * @return void
+  */
+
   void checkEmpty();//Read matrix and check when there is a void on the matrix
+
+  /**
+  * @brief  positionGems(int i, int j) Auxiliar funcion of checkEmpty
+  * @param i, j (Numbers)
+  * @return void
+  */
+
   void positionGems(int i, int j);
+
+  /**
+  * @brief  fill() Function who add new gems on void spaces. 
+  * @param void
+  * @return void
+  */
+
   void fill();//When there is a void on the matrix
+
 //
    int locX, locY; // Auxiliars to move around the Matrix
    char dir; // IDENTIFIER -> 
@@ -29,13 +101,54 @@ public:
    // DOWN -> D
    // RIGHT -> R
    // LEFT -> L
+
+   /**
+  * @brief  Create() Starter function. Generate a random Matrix of gems. 
+  * @param void
+  * @return void
+  */
+
    void Create(); // Generate a random Matrix of gems
+ 
+   /**
+  * @brief  ShowBoard() Function who shows the generated Matrix and number per gem.  
+  * @param void
+  * @return void
+  */
+
    void ShowBoard(); // This function show the generated matrix
+
+   /**
+  * @brief  CountGem() Auxiliar function of ShowBoard, cound each type of gem.   
+  * @param void
+  * @return void
+  */
+
    void CountGem(); // Auxiliar function of ShowBoard, counts 
    // Each type of gem 
+
+   /**
+  * @brief  SaveGame() Save the current Matrix in a .txt file called "File"  
+  * @param void
+  * @return void
+  */
+
    void SaveGame(string Route); // Save the current matrix in a .txt file called "File"
+
+   /**
+  * @brief  LoadGame() Load a Matrix saved in a .txt file called "File" and all the movements and score done.
+  * @param void
+  * @return void
+  */
+
    void LoadGame(string namefile);
    int Movements;
+
+   /**
+  * @brief  possibleMatchs vector who saves all the positions where match is happening. 
+  
+  */
+
    vector <Gem*>  possibleMatchs; // Auxiliar vector of Match()
 
 
@@ -43,6 +156,12 @@ public:
    int y; // Definition of positions of Matrix Recursive -> Match
 
    int score; // The points per gem.
+
+  /**
+  * @brief  MatchMind(int x, int y) Exclusive match for only gems type 6 (Mindgem)
+  * @param x, y Numbers who made together a location on the matrix. 
+  * @return void
+  */
 
    // New Functions
    void MatchMind(int x, int y);  
--- Gem.h
+++ Gem.h
@@ -1,3 +1,11 @@
+/**
+* @file Gem.h
+* @version 1.0
+* @date 08/09/2019
+* @author Grupo04
+* @title Gema
+* @brief Functions who define and explain how gems works 
+*/
 #ifndef _GEM_H_
 #define _GEM_H_
 #include <time.h>
@@ -7,6 +15,13 @@
 class Gem
 {
 public:
+
+/**
+  * @brief  Gem(int ident) Builder method. It receives an identifier to recognize what gem is it
+  * @param ident (number who classify the gem)
+  * @return void
+  */
+
   Gem(int ident); // Builder 
   // it receives and identifier to recognize what gem is it 
   /*
@@ -17,13 +32,55 @@ public:
   5 - SoulGem
   6 - MindGem
   */
+
+  /**
+  * @brief  ~Gem() Destructor
+  * @param void
+  * @return void
+  */
+
   ~Gem(); // Destructor 
+
+  /**
+  * @brief getType() returns the number what identifies the type
+  * @param void
+  * @return number (Type of gem)
+  */
+
   int getType(); // Returns the number what identifies the type
+
+  /**
+  * @brief  setType(int num)  establishes the type of gem with the num added to function
+  * @param num (identifier)
+  * @return void
+  */
+
   void setType(int num);
+
+  /**
+  * @brief  Break() Function is used when a gem is deleted.
+  * @param void
+  * @return void
+  */
+
   void Break();  // This function is used when a gem is deleted.
+
+   /**
+  * @brief Lock() changes mindlock attribute  
+  * @param void
+  * @return void
+  */
+
   void Lock(); // Changes mindLock attribute
 
-  // New functions 
+  // New functions
+
+   /**
+  * @brief  int getMind() Helps to see if a gem its blocked
+  * @param void
+  * @return number (betweem 0 - 2)
+  */
+
   int getMind(); // Helps to see if a gem its blocked 
 
 protected:
